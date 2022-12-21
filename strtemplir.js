9f1341f5078958fe4f8983b708f2cf541fab1436
Your Name <you@example.com>
2022-12-21 10:23:59 +0700

    update

---
+++ strtemplir.js
@@ -0,0 +1,15 @@
+//Concatenation wtih temlate literals
+console.log("\n********Concation with template literals********\n");
+
+let str1 ="JavaScript";
+let str2 = "fun";
+
+console.log('T am writing code in ${str1}');
+console.log('Formatting in ${str1} is ${syr2}!');
+
+//Expressions in template literals
+console.log("\n********Expressions in template litrals********\n");
+
+let bool1 = true;
+console.log(' 1 + 1 is ${1 + 1}');
+console.log('The opposite of true is ${!bool1}');
