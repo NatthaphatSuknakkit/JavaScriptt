d673dcd86eb904cc92376af8ef5b721388d31e7d
Your Name <you@example.com>
2022-12-21 09:43:26 +0700

    datatypes.js

---
+++ Lecture-02/index.js
@@ -0,0 +1,11 @@
+const message ="Hello";
+const place = "World";
+
+//logging out a string
+console.log ('Hello World');
+
+//using substitutions
+console.log ('Hello,%!', place);
+
+//using s stting literal
+console.log ('${message},${place}!');
