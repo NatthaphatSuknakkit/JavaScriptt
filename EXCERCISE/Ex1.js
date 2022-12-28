13a71084628e845df040ff8d6b7c4f4a25bbf860
Your Name <you@example.com>
2022-12-28 10:33:40 +0700

    update

--- EXCERCISE/Ex1.js
+++ EXCERCISE/Ex1.js
@@ -1,6 +1,6 @@
 function findPerimeter(length,width){
     return 2 * (length + width);
 }
-findPerimeter (6 , 7);
-findPerimeter (20, 10);
-findPerimeter (2 , 9);
+console.log (findPerimeter (6 , 7));
+console.log (findPerimeter (20, 10));
+console.log (findPerimeter (2 , 9));
